b1720c65704df2ffaa6845415ca003f8804acc81
Will Marshall <willcodymarshall@gmail.com>
2016-07-25 08:58:40 -0400

    Removed angular dependency. Pure ES2016 + Webpack + Bootstrap

--- Application/app_src/lib/app.js
+++ Application/app_src/lib/app.js
@@ -1,47 +1,5 @@
-var bootstrap = require("bootstrap-sass");
-//test
+import "babel-polyfill";
+import "bootstrap-sass";
 module.exports = {
-  message: "hello world"
-}
 
-console.log("Hello Daniel")
-// (function() {
-//   'use strict';
-
-//   angular
-//     .module('Application')
-//     .config(AppConfig)
-//     .run(AppRun);
-
-//   AppConfig.$inject = ['$stateProvider', '$urlRouterProvider', '$resourceProvider'];
-
-//   function AppConfig($stateProvider, $urlRouterProvider, $resourceProvider) {
-//     $urlRouterProvider.otherwise("/");
-
-//     // Normal analyst states
-//     $stateProvider
-//       .state('dashboard', {
-//         url: '/',
-//         templateUrl: '/static/partials/dashboard.html',
-//       });
-
-//     // Don't strip trailing slashes from calculated URLs
-//     $resourceProvider.defaults.stripTrailingSlashes = false;
-
-//   }
-
-//   AppRun.$inject = ['$rootScope'];
-
-//   function AppRun($rootScope) {
-//     // Expose `_.` methods in any template
-//     $rootScope._ = _;
-
-//     $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-//       // Scroll to top of page on any state change.
-//       // This is coupled with the `autoscroll="false"` on the ui-view tag
-//       // in Application/templates/dashboard.html
-//       $('html, body').animate({scrollTop: 0}, 200);
-
-//     });
-//   }
-// })();
+};
--- Application/app_src/webpack.config.js
+++ Application/app_src/webpack.config.js
@@ -2,20 +2,23 @@ var path = require("path");
 var webpack = require("webpack");
 
 var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin');
-var multi = require("multi-loader");
 
-var rootAssetPath = ".";
+var rootAssetPath = "./";
 var file_loader = "file?context=" + rootAssetPath + "&name=[path][name].[hash].[ext]";
 
 module.exports = {
   entry: {
-    "app_js": "./lib/app.js",
-    "app_style": "./styles/styles.scss",
+    "app_js": ["babel-polyfill", "./lib/app.js"],
+    "app_style": ["./styles/styles.scss"],
   } ,
   output: {
     path: "../static",
     publicPath:"/static/",
-    filename: "[name].[hash].js"
+    filename: "[name].[hash].js",
+    chunkFilename: "[id].[chunkhash].js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".css"]
   },
   // devtool: "source-map",
   module: {
@@ -23,16 +26,23 @@ module.exports = {
       //Sass
       {
         test: /\.s(c|a)ss$/,
+        // TODO: MAKE WORK IN PRODUCTION
         // loader: multi(
-        loaders :
-                      ["style", "css", "resolve-url?fail", "sass?sourceMap"]
-                      // ["file?context=" + rootAssetPath + "&name=[path][name].[hash].css", "extract", "css?sourceMap", "resolve-url", "sass?sourceMap"].join("!")
-                      // )
+        //               ["file?context=" + rootAssetPath + "&name=[path][name].[hash].css", "extract", "css?sourceMap", "resolve-url", "sass?sourceMap"].join("!"),
+        //               ["style", "css", "resolve-url?fail", "sass?sourceMap"].join("!")
+        //               )
+        loaders: ["style", "css?sourceMap", "resolve-url", "sass?sourceMap"]
       },
       //Vue
       {
         test: /\.vue$/,
         loaders: ["vue"]
+      },
+      //JS (ES6)
+      {
+        test: /\.js$/,
+        loaders: ["babel"],
+        exclude: /node_modules/
       },
       //Fonts
       {
@@ -44,7 +54,8 @@ module.exports = {
   plugins:[
     new ManifestRevisionPlugin(path.join(rootAssetPath, "manifest.json"), {
       rootAssetPath: rootAssetPath,
-      ignorePaths: ["node_modules", "webpack.config.js", "manifest.json", "package.json", "partials"]
+      ignorePaths: ["node_modules", "webpack.config.js", "manifest.json", "package.json", ".babelrc"],
+      // extensionsRegex: /\.scss$/
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
